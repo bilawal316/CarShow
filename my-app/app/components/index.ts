e45344612013c25a99883250c7cffdf072a81734
bilawal316 <143614305+bilawal316@users.noreply.github.com>
2023-10-10 19:11:25 +0000

    modified app/components/index.ts, app/page.tsx, types/index.ts

--- my-app/app/components/index.ts
+++ my-app/app/components/index.ts
@@ -1,12 +1,19 @@
 import Hero from "./Hero/hero";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
+import SearchBar from "./SearchBar/SearchBar";
+import CustomFilter from "./CustomFilter/CustomFilter";
+import SearchManufacturer from "./SearchManufacturer/SearchManufacturer";
 import CustomButton from "../components/CustomButton/customButton";
+
 
 export {
     Navbar,
     Hero,
     CustomButton,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
 
 }
